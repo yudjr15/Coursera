8129a1a463501d0ef05e443854880e6a07918ffc
yudjr15 <127331769+yudjr15@users.noreply.github.com>
2023-03-14 10:38:17 +0700

    Add files via upload

--- Create and Share Jupyter Notebook.py
+++ Create and Share Jupyter Notebook.py
@@ -1,2 +1,56 @@
-#Display the output
-print("Create and Share Jupyter No.py")
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Objectives :
+# - List of popular languages for Data Science
+# - List of commonly used librabries for Data Science
+# - List of commonly used tools for Data Science 
+
+# #### Some of the popular languages that Data Scientists use are:
+
+# - Python
+# - R 
+# - SQL
+
+# ##### Some of the commonly used libraries used by Data Scienctists include:
+# - Pandas
+# - Matplotlib
+# - Scikit-learn
+# - Numpy
+
+# ##### Data science tools
+
+# | Data Science Tools |
+# |--------------------|
+# | Python |
+# | R studio|
+# | Power BI|
+
+# ### Below are a few examples of evaluating arithmetics expression in Python
+
+# In[4]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by dividing 60 
+200 // 60 
+
+
+# ## Author 
+# Yudha Siahaan
+
+# In[ ]:
+
+
+
+
